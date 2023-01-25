f3afceb8d82d01ca287ed98a71136fa2b90cb412
Erhan <erhantezer68@gmail.com>
2023-01-25 21:02:07 +0300

    blogcard updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,14 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import BlogCard from '../components/BlogCard'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Grid container  spacing={2} m="auto">
+      <Grid  item xs={6} md={8}>
+        <BlogCard/>
+      </Grid>
+    </Grid>
   )
 }
 
