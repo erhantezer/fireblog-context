52ffe478084f4e948534c7208c25155fa5060ac3
Erhan <erhantezer68@gmail.com>
2023-01-27 15:28:50 +0300

    updateblog added

--- src/pages/UpdateBlog.jsx
+++ src/pages/UpdateBlog.jsx
@@ -1,9 +1,121 @@
-import React from 'react'
 
-const UpdateBlog = () => {
+import { makeStyles } from "@mui/styles";
+import {
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: 90,
+  },
+  paper: {
+    marginTop: 4,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  blogImg: {
+    width: 500,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: 3,
+  },
+  submit: {
+    marginTop: 5,
+    marginBottom: 5,
+    backgroundColor: "#046582",
+    color: "white",
+    fontWeight: "bold",
+  },
+  title: {
+    fontSize: 35,
+    fontFamily: "Girassol",
+    color: "#046582",
+  },
+}));
+
+export default function UpdateBlog() {
+  const classes = useStyles();
+ 
+
+
+
   return (
-    <div>UpdateBlog</div>
-  )
-}
+    <Container component="main" maxWidth="xs" className={classes.root}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <img
+          src={null}
+          alt="blog"
+          className={classes.blogImg}
+        />
+        <Typography component="h1" variant="h5" className={classes.title}>
+          ── Update Blog ──
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="title"
+                label="Title"
+                name="title"
+                
+                // value={newBlog?.title || res?.title}
+                autoFocus
+              
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="image"
+                label="Image URL"
+                
+                type="text"
+                id="image"
+                // value={newBlog?.image || res?.image}
+                
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-multiline-static"
+                required
+                label="Content"
+                multiline
+                
+                // value={newBlog?.content || res?.content}
+                fullWidth
+                rows={15}
+                variant="outlined"
 
-export default UpdateBlog
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className={classes.submit}
+            
+          >
+            Update
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
